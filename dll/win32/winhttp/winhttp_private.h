a26bfce5f5626207aaf05c2b0db9f7db49fcb80e
winesync <ros-dev@reactos.org>
2025-01-03 18:01:50 -0500

    [WINESYNC] winhttp: Stub WINHTTP_OPTION_WEB_SOCKET_RECEIVE_BUFFER_SIZE.
    wine commit id d8552dab2b70f0957d246a28f2e43c8fed7ed92a by Paul Gofman <pgofman@codeweavers.com>

--- dll/win32/winhttp/winhttp_private.h
+++ dll/win32/winhttp/winhttp_private.h
@@ -87,6 +87,7 @@ struct session
     HANDLE unload_event;
     DWORD secure_protocols;
     DWORD passport_flags;
+    unsigned int websocket_receive_buffer_size;
 };
 
 struct connect
@@ -222,6 +223,7 @@ struct request
         WCHAR *username;
         WCHAR *password;
     } creds[TARGET_MAX][SCHEME_MAX];
+    unsigned int websocket_receive_buffer_size;
 };
 
 enum socket_state
