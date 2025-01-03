0df5f669af0b6581540326547df9467df53e155e
winesync <ros-dev@reactos.org>
2025-01-03 17:42:07 -0500

    [WINESYNC] winhttp: Execute WinHttpQueryDataAvailable() synchronously if the data is available.
    Signed-off-by: Paul Gofman <pgofman@codeweavers.com>
    Signed-off-by: Hans Leidekker <hans@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    
    wine commit id 1c659954df22593771c455a731b1bee761fb8742 by Paul Gofman <pgofman@codeweavers.com>

--- dll/win32/winhttp/request.c
+++ dll/win32/winhttp/request.c
@@ -2902,6 +2902,7 @@ BOOL WINAPI WinHttpQueryDataAvailable( HINTERNET hrequest, LPDWORD available )
 {
     DWORD ret;
     struct request *request;
+    BOOL async;
 
     TRACE("%p, %p\n", hrequest, available);
 
@@ -2917,7 +2918,8 @@ BOOL WINAPI WinHttpQueryDataAvailable( HINTERNET hrequest, LPDWORD available )
         return FALSE;
     }
 
-    if (request->connect->hdr.flags & WINHTTP_FLAG_ASYNC)
+    if ((async = request->connect->hdr.flags & WINHTTP_FLAG_ASYNC) && !end_of_read_data( request )
+                                                                   && !query_data_ready( request ))
     {
         struct query_data *q;
 
@@ -2931,12 +2933,13 @@ BOOL WINAPI WinHttpQueryDataAvailable( HINTERNET hrequest, LPDWORD available )
             release_object( &request->hdr );
             free( q );
         }
+        else ret = ERROR_IO_PENDING;
     }
-    else ret = query_data_available( request, available, FALSE );
+    else ret = query_data_available( request, available, async );
 
     release_object( &request->hdr );
     SetLastError( ret );
-    return !ret;
+    return !ret || ret == ERROR_IO_PENDING;
 }
 
 static void CALLBACK task_read_data( TP_CALLBACK_INSTANCE *instance, void *ctx, TP_WORK *work )
