c650d83fdab72b3f45eae0467fdd9dfe10ee224f
winesync <ros-dev@reactos.org>
2025-04-07 17:53:42 -0400

    [WINESYNC] gdiplusenums.h: Remove PaletteFlags.
    It's not present there in Windows SDK.
    
    Signed-off-by: Jacek Caban <jacek@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    
    wine commit id f7ab5c6d792212b0f3e9f4dd5f005f2905fe131e by Jacek Caban <jacek@codeweavers.com>

--- sdk/include/psdk/gdiplusenums.h
+++ sdk/include/psdk/gdiplusenums.h
@@ -792,7 +792,6 @@ typedef enum FontStyle FontStyle;
 typedef enum StringFormatFlags StringFormatFlags;
 typedef enum HotkeyPrefix HotkeyPrefix;
 typedef enum PenAlignment PenAlignment;
-typedef enum PaletteFlags PaletteFlags;
 typedef enum ImageCodecFlags ImageCodecFlags;
 typedef enum EncoderParameterValueType EncoderParameterValueType;
 typedef enum EncoderValue EncoderValue;
