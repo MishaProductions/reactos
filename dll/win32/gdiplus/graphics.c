1388dbf468747b9aa06c8531e06b15784239d6f0
winesync <ros-dev@reactos.org>
2025-04-07 17:56:16 -0400

    [WINESYNC] gdiplus: Add missing allocation failure checks to graphics functions.
    Signed-off-by: Shawn M. Chapla <schapla@codeweavers.com>
    Signed-off-by: Esme Povirk <esme@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    
    wine commit id c155b043fd62e2482d05fa62ad5382d96a2e9823 by Shawn M. Chapla <schapla@codeweavers.com>

--- dll/win32/gdiplus/graphics.c
+++ dll/win32/gdiplus/graphics.c
@@ -5332,7 +5332,14 @@ GpStatus gdip_format_string(HDC hdc,
     }
 
     if (hotkeyprefix_count)
+    {
         hotkeyprefix_offsets = heap_alloc_zero(sizeof(INT) * hotkeyprefix_count);
+        if (!hotkeyprefix_offsets)
+        {
+            heap_free(stringdup);
+            return OutOfMemory;
+        }
+    }
 
     hotkeyprefix_count = 0;
 
@@ -6695,6 +6702,7 @@ GpStatus WINGDIPAPI GdipDrawPolygonI(GpGraphics *graphics,GpPen *pen,GDIPCONST G
 
     if(count<=0)    return InvalidParameter;
     ptf = heap_alloc_zero(sizeof(GpPointF) * count);
+    if (!ptf) return OutOfMemory;
 
     for(i = 0;i < count; i++){
         ptf[i].X = (REAL)points[i].X;
