3560884db70651be7900d1f2658e39ac5bc8a5c7
winesync <ros-dev@reactos.org>
2025-04-07 17:41:07 -0400

    [WINESYNC] gdiplus: Print fixme in GdipGetNearestColor only once.
    Signed-off-by: Dmitry Timoshkov <dmitry@baikal.ru>
    Signed-off-by: Vincent Povirk <vincent@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    
    wine commit id a99cad4ae735143321628e1a2d8e1b87a6d576a0 by Dmitry Timoshkov <dmitry@baikal.ru>

--- dll/win32/gdiplus/graphics.c
+++ dll/win32/gdiplus/graphics.c
@@ -4925,8 +4925,9 @@ GpStatus WINGDIPAPI GdipGetNearestColor(GpGraphics *graphics, ARGB* argb)
 
     if (graphics->image->type == ImageTypeBitmap)
     {
+        static int once;
         GpBitmap *bitmap = (GpBitmap *)graphics->image;
-        if (IsIndexedPixelFormat(bitmap->format))
+        if (IsIndexedPixelFormat(bitmap->format) && !once++)
             FIXME("(%p, %p): Passing color unmodified\n", graphics, argb);
     }
 
